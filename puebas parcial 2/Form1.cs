27a12cb9f7e15a95830149514e7bfe1f807f18d2
TOEE01 <recinos.melvin32@gmail.com>
2021-05-13 15:55:59 -0600

    ejercicio

---
+++ puebas parcial 2/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace puebas_parcial_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                int dato1 = 16;
+                String dato2 = "25g";
+                int resultado = dato1 + Convert.ToInt32(dato2);
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Lo sentimos no se puede realizar la operacionsolicitada" + ex.ToString());
+            }
+            
+        }
+    }
+}
